7d5b23c82beaa7c4d3a05c3f07a01391e542e4e9
jayrajrao <jayrajrao15@gmail.com>
2026-08-15 12:41:04 +0530

    product add

--- src/pages/vendor/AddProduct.jsx
+++ src/pages/vendor/AddProduct.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3910";
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:3910";
 
 function AddProduct() {
   const navigate = useNavigate();
